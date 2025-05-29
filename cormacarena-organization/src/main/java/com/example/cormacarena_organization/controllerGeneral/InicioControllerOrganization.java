ee5a2a609c5e96c05263d156be997fd7ea210b02
stivenzxy <jesuspereztorres420@gmail.com>
2025-05-29 14:03:57 -0500

    Organizacion: index configurado

--- cormacarena-organization/src/main/java/com/example/cormacarena_organization/controllerGeneral/InicioControllerOrganization.java
+++ cormacarena-organization/src/main/java/com/example/cormacarena_organization/controllerGeneral/InicioControllerOrganization.java
@@ -5,10 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class InicioControllerOrganization {
-    @GetMapping("/organizacion")
+    @GetMapping("/")
     public String mostrarInicio() {
         return "index";
     }
-
-
 }
